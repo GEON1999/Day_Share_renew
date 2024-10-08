1dabef2148418451c1111f487147a0e83cb052e6
GEON1999 <phgst12@gmail.com>
2024-10-08 19:05:32 +0900

    필요 데이터 ssr처리

--- src/app/calendar/[id]/page.tsx
+++ src/app/calendar/[id]/page.tsx
@@ -12,14 +12,6 @@ import rqOption from "@/server/rqOption";
 import Helper from "@/helper/Helper";
 import ClientPage from "./clientPage";
 
-const getUser = async (accessToken: any) => {
-  const { data } = await axios.get(
-    `${process.env.BASE_URL}${API.GET_USER}`,
-    rqOption.apiHeader(accessToken)
-  );
-  return data;
-};
-
 const getUserTodos = async (accessToken: any, query: string) => {
   const { data } = await axios.get(
     `${process.env.BASE_URL}${API.GET_USER_TODOS(query)}`,
@@ -28,9 +20,17 @@ const getUserTodos = async (accessToken: any, query: string) => {
   return data;
 };
 
-const getUserDiaries = async (accessToken: any, query: string) => {
+const getCalendarUserInfo = async (accessToken: any, id: string) => {
   const { data } = await axios.get(
-    `${process.env.BASE_URL}${API.GET_USER_DIARIES(query)}`,
+    `${process.env.BASE_URL}${API.GET_CALENDAR_USER_INFO(id)}`,
+    rqOption.apiHeader(accessToken)
+  );
+  return data;
+};
+
+const getCalendarPermissionList = async (accessToken: any, id: string) => {
+  const { data } = await axios.get(
+    `${process.env.BASE_URL}${API.GET_CALENDAR_PERMISSION_LIST(id)}`,
     rqOption.apiHeader(accessToken)
   );
   return data;
@@ -44,32 +44,57 @@ const getCalendarList = async (accessToken: any, query: string) => {
   return data;
 };
 
+const getInviteCode = async (accessToken: any, id: string) => {
+  const { data } = await axios.get(
+    `${process.env.BASE_URL}${API.GET_INVITE_CODE(id)}`,
+    rqOption.apiHeader(accessToken)
+  );
+  return data;
+};
+
+const getCalendarDetail = async (accessToken: any, id: string) => {
+  const { data } = await axios.get(
+    `${process.env.BASE_URL}${API.GET_CALENDAR_DETAIL(id)}`,
+    rqOption.apiHeader(accessToken)
+  );
+  return data;
+};
+
 export default async function Home(req: any) {
   const encryptedAccessToken = cookies().get("AccessToken");
   const accessToken = AesEncryption.aes_decrypt(encryptedAccessToken);
 
   const queries = Helper.queryToString(req.searchParams) ?? "";
-  console.log("queries :", queries);
 
-  console.log("accessToken", accessToken);
   const queryClient = new QueryClient();
 
+  const id = req.params.id;
+  console.log("id", id);
+
   Promise.all([
-    await queryClient.prefetchQuery({
-      queryKey: [QueryKeys.GET_USER],
-      queryFn: () => getUser(accessToken),
-    }),
     await queryClient.prefetchQuery({
       queryKey: [QueryKeys.GET_USER_TODOS, "page=1"],
       queryFn: () => getUserTodos(accessToken, "page=1"),
     }),
     await queryClient.prefetchQuery({
-      queryKey: [QueryKeys.GET_USER_DIARIES, "page=1"],
-      queryFn: () => getUserDiaries(accessToken, "page=1"),
-    }),
-    await queryClient.prefetchQuery({
       queryKey: [QueryKeys.GET_CALENDAR_LIST, queries],
       queryFn: () => getCalendarList(accessToken, queries),
+    }),
+    await queryClient.prefetchQuery({
+      queryKey: [QueryKeys.GET_CALENDAR_USER_INFO, id],
+      queryFn: () => getCalendarUserInfo(accessToken, id),
+    }),
+    await queryClient.prefetchQuery({
+      queryKey: [QueryKeys.GET_CALENDAR_PERMISSION_LIST, id],
+      queryFn: () => getCalendarPermissionList(accessToken, id),
+    }),
+    await queryClient.prefetchQuery({
+      queryKey: [QueryKeys.GET_INVITE_CODE, id],
+      queryFn: () => getInviteCode(accessToken, id),
+    }),
+    await queryClient.prefetchQuery({
+      queryKey: [QueryKeys.GET_CALENDAR_DETAIL, id],
+      queryFn: () => getCalendarDetail(accessToken, id),
     }),
   ]);
 
