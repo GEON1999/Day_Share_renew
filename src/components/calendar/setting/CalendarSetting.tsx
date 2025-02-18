3af887c423b1a68aa1871ae310860514f38616bf
GEON1999 <phgst12@gmail.com>
2025-02-18 19:31:25 +0900

    캘린더 세팅 페이지 모바일 대응

--- src/components/calendar/setting/CalendarSetting.tsx
+++ src/components/calendar/setting/CalendarSetting.tsx
@@ -96,47 +96,47 @@ const CalendarSetting = () => {
     <div className="main_container">
       <div className="flex w-full h-screen justify-center items-center flex-col ">
         <IconSetting className="w-5 h-5" />
-        <h1 className="text-[25px] text-[#2D2D2E]">달력 설정</h1>
+        <h1 className="text-xl text-[#2D2D2E]">달력 설정</h1>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col items-center mt-[35px] noto-sans-text "
+          className="flex flex-col items-center mt-[15px] lg:mt-[35px] noto-sans-text "
         >
           <CalendarImgCrop
             calendarImg={calendarImg}
             setCalendarImg={setCalendarImg}
           />
-          <div className="flex flex-col mt-[20px] text-[20px] noto-sans-text ">
-            <div className="flex flex-col space-y-[13px]">
+          <div className="flex flex-col mt-[15px] lg:mt-[20px] text-lg noto-sans-text ">
+            <div className="flex flex-col space-y-[7px] lg:space-y-[13px]">
               <div className="flex flex-col space-y-[3px] items-start">
-                <label className="text-[20px]">달력 이름</label>
+                <label className="text-lg">달력 이름</label>
                 <input
-                  className="w-[390px] h-[50px] bor px-[19px] rounded-md  focus:outline-none "
+                  className="w-[260px] lg:w-[390px] h-[40px] lg:h-[50px] bor px-[19px] rounded-md  focus:outline-none "
                   defaultValue={calendarData?.name}
                   {...register("name")}
                 />
               </div>
               <div className="flex flex-col space-y-[3px] relative">
-                <label className="text-[20px]">초대코드</label>
+                <label className="text-lg">초대코드</label>
                 <input
                   disabled
-                  className="w-[390px] h-[50px] bor px-[19px] rounded-md  focus:outline-none "
+                  className="w-[260px] lg:w-[390px] h-[40px] lg:h-[50px] bor px-[19px] rounded-md  focus:outline-none "
                   defaultValue={inviteCode?.code}
                   id="inviteCode"
                 />
                 <IconReload
-                  className="absolute right-[16px] bottom-[15px] w-[23px] h-[20px] cur"
+                  className="absolute right-[7px] lg:right-[16px] bottom-[10px] lg:bottom-[15px] w-[23px] h-[20px] cur"
                   onClick={reloadInviteCode}
                 />
               </div>
               <div className="flex flex-col space-y-[3px] relative">
-                <label className="text-[20px]">서버 프로필</label>
+                <label className="text-lg">서버 프로필</label>
                 <input
-                  className="w-[390px] h-[50px] bor px-[19px] rounded-md  focus:outline-none text-opacity-10 cursor-not-allowed bg-white"
+                  className="w-[260px] lg:w-[390px] h-[40px] lg:h-[50px] bor px-[19px] rounded-md  focus:outline-none text-opacity-10 cursor-not-allowed bg-white"
                   defaultValue={calendarUserData?.name ?? ""}
                   disabled
                 />
                 <button
-                  className="absolute right-[16px] bottom-[10px] w-[109px] h-[30px] cur rounded-full border-[0.8px] border-[#49494950] text-[15px] hover:bg-[#49494910] hover:border-[#49494950]"
+                  className="absolute right-[6px] lg:right-[16px] bottom-[5px] lg:bottom-[10px] w-[109px] h-[30px] cur rounded-full border-[0.8px] border-[#49494950] text-base hover:bg-[#49494910] hover:border-[#49494950]"
                   onClick={handleChangeProfile}
                   type="button"
                 >
@@ -146,7 +146,7 @@ const CalendarSetting = () => {
             </div>
             <button
               type="submit"
-              className="mt-[40px] rounded-md w-[390px] h-[50px] flex justify-center items-center bor btn_hilight"
+              className="mt-[20px] lg:mt-[40px] rounded-md w-[260px] lg:w-[390px] h-[40px] lg:h-[50px] flex justify-center items-center bor btn_hilight"
             >
               저장
             </button>
@@ -154,7 +154,7 @@ const CalendarSetting = () => {
           <button
             type="button"
             onClick={() => setIsOpen(true)}
-            className="mt-[15px] text-[15px]"
+            className="mt-[15px] text-base"
           >
             서버 나가기
           </button>
