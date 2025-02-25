adc90fba551525c518999ba7398ad1a6671ff4c4
GEON1999 <phgst12@gmail.com>
2025-02-26 02:58:24 +0900

    소개 페이지 마크업

---
+++ src/app/data/slideData.ts
@@ -0,0 +1,33 @@
+export interface SlideData {
+  img: string;
+  alt: string;
+  title: string;
+  text: string;
+}
+
+export const slideData: SlideData[] = [
+  {
+    img: "/images/info_1.png",
+    alt: "dayshare_info_1",
+    title: "DAYSHARE와 함께 일정을 공유해요!",
+    text: "당신의 하루를 소중한 사람과 연결해 줍니다.",
+  },
+  {
+    img: "/images/info_2.png",
+    alt: "dayshare_info_2",
+    title: "지금 가입하세요",
+    text: "지금 가입하세요",
+  },
+  {
+    img: "/images/info_3.png",
+    alt: "dayshare_info_3",
+    title: "지금 가입하세요",
+    text: "지금 가입하세요",
+  },
+  {
+    img: "/images/info_4.png",
+    alt: "dayshare_info_4",
+    title: "지금 가입하세요",
+    text: "지금 가입하세요",
+  },
+];
