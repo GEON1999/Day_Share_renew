d530aa3f21cc377d10f5eef4d6be4a8d684ae1e6
GEON1999 <phgst12@gmail.com>
2025-02-28 16:54:09 +0900

    로딩 스피너 중앙 위치

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Loading() {
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center bg_main">
-      <div className="loading spinner"></div>
+      <div className="flex items-center justify-center">
+        <div className="loading spinner"></div>
+      </div>
     </div>
   );
 }
