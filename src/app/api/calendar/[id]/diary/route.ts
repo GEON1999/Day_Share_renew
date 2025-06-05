73bd1af3b32e5d652f001a38647759886af3bb85
GEON1999 <phgst12@gmail.com>
2025-06-05 22:38:57 +0900

    feat: 캘린더 다이어리 API 엔드포인트 복구

---
+++ src/app/api/calendar/[id]/diary/route.ts
@@ -0,0 +1,73 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+import API from "@/server/API";
+import rqOption from "@/server/rqOption";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
+export async function GET(req: any, res: { params: { id?: string } }) {
+  const session = await getServerSession(authOptions);
+  const accessToken = session?.accessToken;
+
+  if (accessToken === undefined) {
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
+
+  const id = res.params.id;
+  if (!id) {
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
+  const queries = `${req.nextUrl.searchParams.toString()}`;
+
+  try {
+    const data = await axios.get(
+      `${process.env.BASE_URL}${API.GET_DIARIES(id, queries)}`,
+      rqOption.apiHeader(accessToken)
+    );
+
+    return NextResponse.json(data.data, { status: 200 });
+  } catch (error) {
+    console.error("API call error:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: any, res: { params: { id?: string } }) {
+  const session = await getServerSession(authOptions);
+  const accessToken = session?.accessToken;
+
+  if (accessToken === undefined) {
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
+  const id = res.params.id;
+  const body = await req.json();
+  const queries = `${req.nextUrl.searchParams.toString()}`;
+
+  try {
+    const data = await axios.post(
+      `${process.env.BASE_URL}${API.CREATE_DIARY(id, queries)}`,
+      body,
+      rqOption.apiHeader(accessToken)
+    );
+
+    return NextResponse.json(data.data, { status: 200 });
+  } catch (error) {
+    console.error("API call error:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
+}
